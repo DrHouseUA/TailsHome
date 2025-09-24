ec9dd9b38c8a0660709e598930bb40e0917dab3d
DrHouseUA <romanknyazhyk@gmail.com>
2025-09-24 19:05:41 +0000

    Deploying to gh-pages from @ DrHouseUA/TailsHome@b239d674f8f95f2bd25e3d7f3cfcac9c72df6463 🚀

--- index.js
+++ index.js
@@ -1,72 +1,72 @@
-import{a as l,i as C,S as h,A as q}from"./assets/vendor-B6gZsb19.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const n of document.querySelectorAll('link[rel="modulepreload"]'))o(n);new MutationObserver(n=>{for(const a of n)if(a.type==="childList")for(const c of a.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&o(c)}).observe(document,{childList:!0,subtree:!0});function s(n){const a={};return n.integrity&&(a.integrity=n.integrity),n.referrerPolicy&&(a.referrerPolicy=n.referrerPolicy),n.crossOrigin==="use-credentials"?a.credentials="include":n.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function o(n){if(n.ep)return;n.ep=!0;const a=s(n);fetch(n.href,a)}})();function B(){return window.innerWidth>=1440?9:8}function T(t,e){return t==="Всі"?e:e.filter(s=>s.categories.some(o=>o.name===t))}function P(t,e){return e.filter(o=>o._id===t)}const b=document.querySelector(".gallery"),f=document.querySelector(".category-nav"),y=document.querySelector(".showmore-btn");let L="Всі",r=1;function I(){b.innerHTML=""}function O(t){t.forEach(e=>{const s=document.createElement("article");s.classList.add("pet-card"),s.innerHTML=`
+import{a as u,i as x,S,A}from"./assets/vendor-B6gZsb19.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const n of document.querySelectorAll('link[rel="modulepreload"]'))s(n);new MutationObserver(n=>{for(const r of n)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&s(a)}).observe(document,{childList:!0,subtree:!0});function t(n){const r={};return n.integrity&&(r.integrity=n.integrity),n.referrerPolicy&&(r.referrerPolicy=n.referrerPolicy),n.crossOrigin==="use-credentials"?r.credentials="include":n.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(n){if(n.ep)return;n.ep=!0;const r=t(n);fetch(n.href,r)}})();function O(){return window.innerWidth>=1440?9:8}function D(e,o){return e==="Всі"?o:o.filter(t=>t.categories.some(s=>s.name===e))}function N(e,o){return o.filter(s=>s._id===e)}function y(e){console.log(e),e.classList.remove("hidden")}function b(e){e.classList.add("hidden")}const k=document.querySelector(".gallery"),L=document.querySelector(".category-nav"),l=document.querySelector(".showmore-btn"),m=document.querySelector(".loader-cards-backdrop");console.log(m);let q="Всі",c=1;function H(){k.innerHTML=""}function R(e,o=!1){e.forEach(t=>{const s=document.createElement("article");s.classList.add("pet-card"),o&&s.classList.add("new-card"),s.innerHTML=`
       <div class="card-img-wrapper">
-        <img class="pet-card-image" src="${e.image||"default.jpg"}" alt="Фото ${e.name}, ${e.age}, ${e.gender}" loading="lazy"/>
+        <img class="pet-card-image" src="${t.image||"default.jpg"}" alt="Фото ${t.name}, ${t.age}, ${t.gender}" loading="lazy"/>
       </div>
       <div class="pet-card-info">
-        <span class="pet-card-type">${e.species}</span>
-        <h3 class="pet-card-name">${e.name}</h3>
-       <span class="pet-category-wrapper">${A(e.categories)}</span>
+        <span class="pet-card-type">${t.species}</span>
+        <h3 class="pet-card-name">${t.name}</h3>
+        <span class="pet-category-wrapper">${V(t.categories)}</span>
         <div class="pet-card-meta">
-          <p class="pet-card-age">${e.age}</p>
-          <p class="pet-card-gender">${e.gender}</p>
+          <p class="pet-card-age">${t.age}</p>
+          <p class="pet-card-gender">${t.gender}</p>
         </div>
       </div>
-      <p class="pet-card-desc">${e.shortDescription}</p>
-      <button class="pet-card-btn" id="${e._id}">Дізнатись більше</button>
-    `,b.appendChild(s)})}function A(t){let e="";for(const s of t)e+=`<span class="pet-category">
-      ${s.name}
-    </span>`;return e}function x(t=1){const e=P(t,u);let s="";return e.forEach(o=>s=`
+      <p class="pet-card-desc">${t.shortDescription}</p>
+      <button class="pet-card-btn" id="${t._id}">Дізнатись більше</button>
+    `,k.appendChild(s)})}function V(e){let o="";for(const t of e)o+=`<span class="pet-category">
+      ${t.name}
+    </span>`;return o}function j(e=1){const o=N(e,f);let t="";return o.forEach(s=>t=`
         <div class="detail-image-wrapper">
-          <img class="image-tamplete" src="${o.image}"></img>
+          <img class="image-tamplete" src="${s.image}"></img>
         </div>
         <div class="detail-descr-wrapper">
           <div class="detail-descr-title">
-            <p>${o.species}</p>
-            <h1 class="detail-pet-name">${o.name}</h1>
-            <p>${o.age}&nbsp;&nbsp;&nbsp;&nbsp;  ${o.gender}</p>
+            <p>${s.species}</p>
+            <h1 class="detail-pet-name">${s.name}</h1>
+            <p>${s.age}&nbsp;&nbsp;&nbsp;&nbsp;  ${s.gender}</p>
                     <button type="button" class="showmore-btn take-home-btn take-home-btn-tablet">
             Взяти додому
           </button>
           </div>
           <p><span class="modal-txt">Опис:  <br /></span>
-          ${o.description}
+          ${s.description}
           </p>
           <p><span class="modal-txt">Здоров'я:  <br /></span> 
-          ${o.healthStatus}
+          ${s.healthStatus}
           </p>
           <p><span class="modal-txt">Поведінка:</span> <br />
-          ${o.behavior}
+          ${s.behavior}
           </p>
           <button type="button" class="showmore-btn take-home-btn">
             Взяти додому
           </button>
         </div>
-  `),s}function H(t){f.innerHTML="",t.forEach(e=>{const s=document.createElement("button");s.classList.add("category-item"),s.textContent=e,s.dataset.category=e,e==="Всі"&&s.classList.add("active-category"),s.addEventListener("click",()=>{document.querySelectorAll(".category-item").forEach(o=>o.classList.remove("active-category")),s.classList.add("active-category"),L=e,r=1,d()}),f.appendChild(s)})}function d(){const t=B(),e=T(L,u),s=e.slice(0,r*t);I(),O(s);const o=Math.ceil(e.length/t);y.style.display=r>=o?"none":"block"}y.addEventListener("click",()=>{r+=1,d()});const w=l.create({baseURL:"https://paw-hut.b.goit.study/",timeout:5e3,headers:{accept:"application/json"}});let u=[];async function D(){let e=1,s=[];try{for(;;){const n=(await w.get("api/animals",{params:{page:e,limit:30}})).data.animals||[];if(s=[...s,...n],n.length<30)break;e++}u=s,d()}catch(o){C.error({title:"Помилка",message:"Не вдалося завантажити тваринок.",position:"topRight"}),console.error("Axios error:",o)}}async function N(){try{const e=(await w.get("api/categories")).data||[];H(["Всі",...e.map(s=>s.name)])}catch(t){console.error("Помилка завантаження категорій:",t)}}new h(".swiper-about",{navigation:{nextEl:".swiper-about-button-next",prevEl:".swiper-about-button-prev"},pagination:{el:".swiper-about-pagination",type:"bullets",clickable:!0}});document.addEventListener("DOMContentLoaded",()=>{new q(".accordion-container",{duration:400,showMultiple:!1,collapse:!0,ariaEnabled:!0})});document.addEventListener("click",t=>{const e=t.target.closest(".ac-trigger");e&&setTimeout(()=>{window.innerWidth<768&&e.scrollIntoView({behavior:"smooth",block:"start"})},450)});const m=document.getElementById("mobileMenu"),j=document.querySelector(".mobile-menu-btn"),V=document.getElementById("menuCloseBtn");j.addEventListener("click",()=>{m.classList.add("active")});V.addEventListener("click",()=>{m.classList.remove("active")});const g=document.querySelectorAll(".nav-link");g.forEach(t=>{t.addEventListener("click",function(e){e.preventDefault(),g.forEach(s=>s.classList.remove("active")),this.classList.add("active")})});document.querySelectorAll(".mobile-nav-btn, .header-btn").forEach(t=>{t.addEventListener("click",()=>{const e=document.querySelector("#pets-list");e&&e.scrollIntoView({behavior:"smooth",block:"start"}),m.classList.remove("active")})});document.querySelectorAll(".mobile-nav-link, .nav-link").forEach(t=>{t.addEventListener("click",e=>{e.preventDefault();const s=t.getAttribute("href");if(s&&s.startsWith("#")){const n=document.querySelector(s);n&&n.scrollIntoView({behavior:"smooth",block:"start"})}document.getElementById("mobileMenu").classList.remove("active")})});const R=document.querySelector(".gallery"),E=document.querySelector(".modal-detail"),k=document.querySelector(".detail-content"),S=document.querySelector("body");R.addEventListener("click",U);document.addEventListener("keydown",function(t){t.key==="Escape"&&p()});function U(t){t.target.nodeName==="BUTTON"&&(k.insertAdjacentHTML("beforeend",x(t.target.attributes.id.value)),F())}function F(){S.addEventListener("click",M),E.classList.remove("hide-modal"),document.body.classList.add("modal-open")}function M(t){(document.body.classList.contains("modal-open")&&t.target.nodeName==="SECTION"||t.target.classList.contains("detail-icon-close"))&&p()}function p(){E.classList.add("hide-modal"),document.body.classList.remove("modal-open"),S.removeEventListener("click",M),k.innerHTML=""}const i=document.querySelector(".modal-order"),W=document.querySelector(".modal-btn"),v=document.querySelector(".modal-detail");v.addEventListener("click",t=>{t.target.nodeName==="BUTTON"&&(i.classList.add("is-open-order"),document.body.classList.add("modal-order","is-open-order"),v.classList.add("hide-modal"))});W.addEventListener("click",$);i.addEventListener("click",t=>{const e=!t.target.closest(".modal"),s=i.classList.contains("is-open-order");e&&s&&$()});function $(){i.classList.remove("is-open-order"),document.body.classList.remove("modal-order","is-open-order"),p()}function z(){return l.defaults.baseURL="https://paw-hut.b.goit.study/",l.get("/api/feedbacks").then(t=>t.data.feedbacks).catch(t=>{throw console.error("Ошибка при получении данных:",t),t})}function _(t){const e=document.querySelector(".stories-swiper .swiper-wrapper"),s=t.map(o=>`
+  `),t}function F(e){L.innerHTML="",e.forEach(o=>{const t=document.createElement("button");t.classList.add("category-item"),t.textContent=o,t.dataset.category=o,o==="Всі"&&t.classList.add("active-category"),t.addEventListener("click",()=>{document.querySelectorAll(".category-item").forEach(s=>s.classList.remove("active-category")),t.classList.add("active-category"),q=o,c=1,p()}),L.appendChild(t)})}function p({append:e=!1}={}){const o=O(),t=D(q,f),s=t.slice(0,c*o);e||H();const n=(c-1)*o,r=s.slice(n,c*o);R(e?r:s,e);const a=Math.ceil(t.length/o);l.style.display=c>=a?"none":"block"}l.addEventListener("click",()=>{c+=1,y(m),b(l),document.querySelectorAll(".new-card").forEach(e=>e.classList.remove("new-card")),p({append:!0}),setTimeout(()=>b(m),200),setTimeout(K(74),200),setTimeout(()=>y(l),200)});function K(e=74){const o=document.querySelector(".gallery .new-card");if(!o)return;const t=o.getBoundingClientRect().top+window.pageYOffset;window.scrollTo({top:t-e,behavior:"smooth"})}const M=u.create({baseURL:"https://paw-hut.b.goit.study/",timeout:5e3,headers:{accept:"application/json"}});let f=[];async function U(){try{let o=1,t=[];for(;;){const s=Array.from({length:5},(a,P)=>M.get("api/animals",{params:{page:o+P,limit:20}})),r=(await Promise.all(s)).flatMap(a=>a.data.animals||[]);if(t=[...t,...r],r.length<20*s.length)break;o+=s.length}f=t,p()}catch(e){x.error({title:"Помилка",message:"Не вдалося завантажити тваринок.",position:"topRight"}),console.error("Axios error:",e)}}async function W(){try{const o=(await M.get("api/categories")).data||[];F(["Всі",...o.map(t=>t.name)])}catch(e){console.error("Помилка завантаження категорій:",e)}}new S(".swiper-about",{navigation:{nextEl:".swiper-about-button-next",prevEl:".swiper-about-button-prev"},pagination:{el:".swiper-about-pagination",type:"bullets",clickable:!0}});document.addEventListener("DOMContentLoaded",()=>{new A(".accordion-container",{duration:400,showMultiple:!1,collapse:!0,ariaEnabled:!0})});document.addEventListener("click",e=>{const o=e.target.closest(".ac-trigger");o&&setTimeout(()=>{window.innerWidth<768&&o.scrollIntoView({behavior:"smooth",block:"start"})},450)});const g=document.getElementById("mobileMenu"),_=document.querySelector(".mobile-menu-btn"),z=document.getElementById("menuCloseBtn");_.addEventListener("click",()=>{g.classList.add("active")});z.addEventListener("click",()=>{g.classList.remove("active")});const w=document.querySelectorAll(".nav-link");w.forEach(e=>{e.addEventListener("click",function(o){o.preventDefault(),w.forEach(t=>t.classList.remove("active")),this.classList.add("active")})});document.querySelectorAll(".mobile-nav-btn, .header-btn").forEach(e=>{e.addEventListener("click",()=>{const o=document.querySelector("#pets-list");o&&o.scrollIntoView({behavior:"smooth",block:"start"}),g.classList.remove("active")})});document.querySelectorAll(".mobile-nav-link, .nav-link").forEach(e=>{e.addEventListener("click",o=>{o.preventDefault();const t=e.getAttribute("href");if(t&&t.startsWith("#")){const n=document.querySelector(t);n&&n.scrollIntoView({behavior:"smooth",block:"start"})}document.getElementById("mobileMenu").classList.remove("active")})});const G=document.querySelector(".gallery"),T=document.querySelector(".modal-detail"),$=document.querySelector(".detail-content"),C=document.querySelector("body");G.addEventListener("click",J);document.addEventListener("keydown",function(e){e.key==="Escape"&&h()});function J(e){e.target.nodeName==="BUTTON"&&($.insertAdjacentHTML("beforeend",j(e.target.attributes.id.value)),Y())}function Y(){C.addEventListener("click",B),T.classList.remove("hide-modal"),document.body.classList.add("modal-open")}function B(e){(document.body.classList.contains("modal-open")&&e.target.nodeName==="SECTION"||e.target.classList.contains("detail-icon-close"))&&h()}function h(){T.classList.add("hide-modal"),document.body.classList.remove("modal-open"),C.removeEventListener("click",B),$.innerHTML=""}const d=document.querySelector(".modal-order"),Z=document.querySelector(".modal-btn"),E=document.querySelector(".modal-detail"),i=document.querySelector(".modal-form");document.querySelector("#form-loader");const Q="https://script.google.com/macros/s/AKfycbxvMWn2o1KlD0kaVSKKSwRLIwg2s2lHDLJmxp62IilpEI-At8MMNph678NHE8LfZELkVA/exec";E.addEventListener("click",e=>{e.target.nodeName==="BUTTON"&&(d.classList.add("is-open-order"),document.body.classList.add("modal-order","is-open-order"),E.classList.add("hide-modal"))});Z.addEventListener("click",v);d.addEventListener("click",e=>{const o=!e.target.closest(".modal"),t=d.classList.contains("is-open-order");o&&t&&v()});function v(){d.classList.remove("is-open-order"),document.body.classList.remove("modal-order","is-open-order"),h()}i.addEventListener("submit",e=>{e.preventDefault();const o=i.querySelector("#user-name").value.trim(),t=i.querySelector("#user-phone").value.trim(),s=i.querySelector("#user-comment").value.trim();if(!o||o.length<2){alert("Введіть коректне ім’я (мінімум 2 символи)");return}if(!/^\+?\d{10,15}$/.test(t)){alert("Введіть коректний номер телефону (наприклад, +380991234567)");return}if(s.length>300){alert("Коментар занадто довгий (максимум 300 символів)");return}const r=new FormData;r.append("name",o),r.append("phone",t),r.append("comment",s),X(),fetch(Q,{method:"POST",body:r}).then(a=>a.text()).then(a=>{alert("Заявка надіслана!"),i.reset(),v()}).catch(a=>{alert("Помилка: "+a.message)}).finally(()=>{ee()})});const I=document.querySelector("#loader-backdrop");function X(){I.classList.remove("hidden")}function ee(){I.classList.add("hidden")}function te(){return u.defaults.baseURL="https://paw-hut.b.goit.study/",u.get("/api/feedbacks").then(e=>e.data.feedbacks).catch(e=>{throw console.error("Ошибка при получении данных:",e),e})}function oe(e){const o=document.querySelector(".stories-swiper .swiper-wrapper"),t=e.map(s=>`
       <div class="swiper-slide swiper-comment-slide">
-        <div class="rating value-${o.rate} star-svg half medium">
+        <div class="rating value-${s.rate} star-svg half medium">
           <div class="star-container">
-            ${G(o.rate)}
+            ${se(s.rate)}
           </div>
         </div>
         <div class="stories-comment-wrapper">
-          <p class="description-comment">${o.description}</p>
-          <p class="author-comment">${o.author}</p>
+          <p class="description-comment">${s.description}</p>
+          <p class="author-comment">${s.author}</p>
         </div>
       </div>
-    `).join("");e.insertAdjacentHTML("beforeend",s)}z().then(t=>{_(t),new h(".stories-swiper",{loop:!1,slidesPerView:1,navigation:{nextEl:".swiper-comment-button-next",prevEl:".swiper-comment-button-prev"},pagination:{el:".stories-pagination",clickable:!0,dynamicBullets:!0},breakpoints:{768:{slidesPerView:2,spaceBetween:32}}})});function G(t){const e=Math.floor(t),s=t%1>=.5,o=5-e-(s?1:0);let n="";for(let a=0;a<e;a++)n+=`
+    `).join("");o.insertAdjacentHTML("beforeend",t)}te().then(e=>{oe(e),new S(".stories-swiper",{loop:!1,slidesPerView:1,navigation:{nextEl:".swiper-comment-button-next",prevEl:".swiper-comment-button-prev"},pagination:{el:".stories-pagination",clickable:!0,dynamicBullets:!0},breakpoints:{768:{slidesPerView:2,spaceBetween:32}}})});function se(e){const o=Math.floor(e),t=e%1>=.5,s=5-o-(t?1:0);let n="";for(let r=0;r<o;r++)n+=`
       <div class="star">
         <svg class="star-filled" width="20" height="19">
           <use href="/TailsHome/icons.svg#icon-star-filled"></use>
         </svg>
-      </div>`;s&&(n+=`
+      </div>`;t&&(n+=`
       <div class="star">
         <svg class="star-half" width="20" height="19">
           <use href="/TailsHome/icons.svg#icon-star-half"></use>
         </svg>
-      </div>`);for(let a=0;a<o;a++)n+=`
+      </div>`);for(let r=0;r<s;r++)n+=`
       <div class="star">
         <svg class="star-empty" width="20" height="19">
           <use href="/TailsHome/icons.svg#icon-star-outline"></use>
         </svg>
-      </div>`;return n}N();D();
+      </div>`;return n}W();U();
 //# sourceMappingURL=index.js.map
